b464709d4a6ad4f6dc33325d951caad0248157aa
Kasim ŞEN <kasim1411@gmail.com>
2019-07-25 01:25:57 +0300

    Updates

---
+++ precache-manifest.f6a49e420856ed191258f160d0bcf2e8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ccb936d6bc0bce04fbb3b976c66fe6",
+    "url": "/StarWars-App/index.html"
+  },
+  {
+    "revision": "9c5f227f36d2b51f9bd8",
+    "url": "/StarWars-App/static/css/main.76d8d9a9.chunk.css"
+  },
+  {
+    "revision": "d2025e408fe1968985cd",
+    "url": "/StarWars-App/static/js/2.72f27249.chunk.js"
+  },
+  {
+    "revision": "9c5f227f36d2b51f9bd8",
+    "url": "/StarWars-App/static/js/main.2d2b44b9.chunk.js"
+  },
+  {
+    "revision": "b7edd3bd3e3afc36ad64",
+    "url": "/StarWars-App/static/js/runtime~main.2676381e.js"
+  },
+  {
+    "revision": "7cb0ec515a22143c7aeed6bbae1561ca",
+    "url": "/StarWars-App/static/media/C-3PO.7cb0ec51.webp"
+  },
+  {
+    "revision": "8d7da71cf8c1ff8a4f6ed5a4a88497b4",
+    "url": "/StarWars-App/static/media/Darth-Vader.8d7da71c.webp"
+  },
+  {
+    "revision": "c10dcdeb9d758da3cc20e80b1c857d97",
+    "url": "/StarWars-App/static/media/Star-Wars-Attack-Clones-II-Poster.c10dcdeb.jpeg"
+  },
+  {
+    "revision": "c17b8220989772528441209d3c472f44",
+    "url": "/StarWars-App/static/media/Star-Wars-New-Hope-IV-Poster.c17b8220.webp"
+  },
+  {
+    "revision": "20dad0591c28c8e615f6292c0891bc17",
+    "url": "/StarWars-App/static/media/Star-Wars-Phantom-Menace-I-Poster.20dad059.webp"
+  },
+  {
+    "revision": "d2d262d64630c0adef087af47dfb9eb7",
+    "url": "/StarWars-App/static/media/Star-Wars-Revenge-Sith-III-Poster.d2d262d6.webp"
+  },
+  {
+    "revision": "ae2b8b90684627d2089218d7eb1ab01c",
+    "url": "/StarWars-App/static/media/luke-skywalker.ae2b8b90.jpeg"
+  },
+  {
+    "revision": "3d0426fc29579251e5475fda8aebaa2b",
+    "url": "/StarWars-App/static/media/r2-d2.3d0426fc.jpeg"
+  },
+  {
+    "revision": "bdeb1fac038b462945d4c3c1fa13a593",
+    "url": "/StarWars-App/static/media/star-wars-banner.bdeb1fac.jpg"
+  }
+]);
